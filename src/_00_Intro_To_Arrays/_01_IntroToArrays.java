7bf0fba3ad1e0a5b4aaf0ca13a8cc65d13ec521a
league <league@192.168.1.200>
2024-06-08 11:30:02 -0700

    start of level 3

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,32 +5,58 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] str = {"cab","osteoporsis","what","alaska","double trouble"};
         // 2. print the third element in the array
-
+    	System.out.println(str[2]);
         // 3. set the third element to a different value
-
+    	str[2] = "cruise";
         // 4. print the third element again
-
+    	System.out.println(str[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	for(int i = 0; i < str.length; i++) {
+    		str[i] = ""+(i+1);
+        	System.out.println(str[i]);
+    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
 
         // 7. make an array of 50 integers
-
+    	int[] holymoly = new int[50];
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+    	Random ran = new Random();
+    	for(int i = 0; i < 50; i++) {
+    		holymoly[i] = ran.nextInt(100);
+    	}
+    	int smallest = 100;
+    	for(int ali : holymoly) {
+    		if(ali < smallest) {
+    			smallest = ali;
+    		}
+    	}
+    	System.out.println(smallest);
+    	
+    	for(int i = 0; i < 50; i++) {
+    		System.out.print(holymoly[i] + " ");
+    	}
+    	
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+    	
         // 10 print the entire array to see if step 8 was correct
 
         // 11. print the largest number in the array.
-
+      	System.out.println("   ");
+    	int largest = -1;
+    	for(int ali : holymoly) {
+    		if(ali > largest) {
+    			largest = ali;
+    		}
+    	}
+    	System.out.println(largest);
+    	
         // 12. print only the last element in the array
-
+    	System.out.println(holymoly[holymoly.length-1]);
     }
 }
--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -8,16 +8,21 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
     // 1. make a main method
-
+    public static void main(String[] args) {
         // 2. create an array of 5 robots.
-
+    	Robot[] robs = new Robot[5];
         // 3. use a for loop to initialize the robots.
-
+    	for(int i = 0; i < robs.length; i++) {
+    		robs[i] = new Robot();
+    		robs[i].setY(200);
+    	}
         // 4. make each robot start at the bottom of the screen, side by side, facing up
     
         // 5. use another for loop to iterate through the array and make each robot move
         // a random amount less than 50.
-    
+    for(int i =0; i < robs.length; i++) {
+    	
+    }
         // 6. use a while loop to repeat step 5 until a robot has reached the top of the
         // screen.
     
@@ -26,5 +31,6 @@ public class _02_RobotRace {
         // 8. try different races with different amounts of robots.
     
         // 9. make the robots race around a circular track.
-
+    	
+    }
 }
